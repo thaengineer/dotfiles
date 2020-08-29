11f286cf21876f4a92754b7c824b67a87a42a42c
thaengineer <51303308+thaengineer@users.noreply.github.com>
2020-08-29 17:35:48 -0400

    Delete display.sh

--- .config/openbox/display.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-intern=LVDS1
-extern=HDMI1
-
-if xrandr | grep "$extern disconnected"; then
-	xrandr --output "$intern" --mode 1366x768 --rate 60.07 --output "$extern" --off
-else
-	xrandr --output "$intern" --primary --mode 1366x768 --rate 60.07 --output "$extern" --right-of "$intern" --auto
-fi
